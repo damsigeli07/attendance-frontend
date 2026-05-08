d6ca9a3f21f84bc91748c8183fc854fc5c9fbdbe
Damsi Jayawarna <damsijayawarna7@gmail.com>
2026-05-08 15:07:28 +0530

    Define application routes for all feature components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { EmployeeListComponent } from './components/employee-list/employee-list';
+import { AddEmployeeComponent } from './components/add-employee/add-employee';
+import { MarkAttendanceComponent } from './components/mark-attendance/mark-attendance';
+import { AttendanceReportComponent } from './components/attendance-report/attendance-report';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'employees', pathMatch: 'full' },
+  { path: 'employees', component: EmployeeListComponent },
+  { path: 'add-employee', component: AddEmployeeComponent },
+  { path: 'mark-attendance', component: MarkAttendanceComponent },
+  { path: 'report', component: AttendanceReportComponent }
+];
