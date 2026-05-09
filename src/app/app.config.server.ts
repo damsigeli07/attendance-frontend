ca02cd79f30b11a5613d7ef5fbb9c12221cd4683
Damsi Jayawarna <damsijayawarna7@gmail.com>
2026-05-09 12:55:00 +0530

    Add server-side configuration files

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,11 +1,12 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering, withRoutes } from '@angular/ssr';
+import { provideServerRendering } from '@angular/platform-server';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering(withRoutes(serverRoutes))
+    provideServerRendering(),
+    provideHttpClient(withFetch())
   ]
 };
 
